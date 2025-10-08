f5f9142167db2e0d27a745782ad300d299d3573c
BK-KRISH <baranikrishnan007@gmail.com>
2025-10-08 10:39:42 +0530

    changed text

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Welcome to shopeasy");
+  res.send("Welcome to ShopEasy ! From App.js");
 });
 
 app.listen(PORT, () => {
